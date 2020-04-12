dbc7a4b06e96448846592dfea3712a873f3c431d
Alejandro Ríos Jasso <jassoalex97@hotmail.com>
2020-04-12 16:10:46 -0500

    Client setup and cleanup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootswatch/dist/minty/bootstrap.min.css';
+import './App.css';
+
+const App = () => {
+	return (
+		<div className='jumbotron'>
+			<div className='container'>
+				<h1 className='text-center'>Mini URL</h1>
+			</div>
+		</div>
+	);
+};
+
+export default App;
